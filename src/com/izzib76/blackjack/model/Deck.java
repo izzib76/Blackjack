6e46dcc9c094a5b7282db47cab68f748c4b51cb9
Izzi Bikun <izzi.bikun@kroger.com>
2018-07-22 11:25:42 -0600

    BLKJK-001 | Izzi | Create deck model

--- src/com/izzib76/blackjack/model/Deck.java
+++ src/com/izzib76/blackjack/model/Deck.java
@@ -1,4 +1,15 @@
 package com.izzib76.blackjack.model;
 
+import java.util.List;
+
 public class Deck {
+    private List<String> cards;
+
+    public List<String> getCards() {
+        return cards;
+    }
+
+    public void setCards(List<String> cards) {
+        this.cards = cards;
+    }
 }
