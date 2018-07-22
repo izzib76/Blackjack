971489b804948437306771a76f2eb7c69b82b38e
Izzi Bikun <izzi.bikun@kroger.com>
2018-07-22 11:27:18 -0600

    BLKJK-002 | Izzi | Create hand model

---
+++ src/com/izzib76/blackjack/model/Hand.java
@@ -0,0 +1,15 @@
+package com.izzib76.blackjack.model;
+
+import java.util.List;
+
+public class Hand {
+    private List<String> cards;
+
+    public List<String> getCards() {
+        return cards;
+    }
+
+    public void setCards(List<String> cards) {
+        this.cards = cards;
+    }
+}
