c2cf6c03c4ee723619a399ac7d924f0c1e96af4d
Izzi Bikun <izzi.bikun@kroger.com>
2018-08-23 16:24:00 -0400

    BLKJK-007 | Izzi | Calculate point value of hand, accounting for Aces

--- src/com/izzib76/blackjack/model/Hand.java
+++ src/com/izzib76/blackjack/model/Hand.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Hand {
     private List<Card> cards;
     private int playerId;
+    private int pointValue;
 
     public int getPlayerId() {
         return playerId;
@@ -25,6 +26,23 @@ public class Hand {
     public String toString() {
         StringBuilder hand = new StringBuilder("Hand for player " + this.playerId + ": ");
         this.cards.forEach(card -> hand.append(card.toString() + " "));
+        hand.append("\nPoint Value of Hand: " + calculatePointValue());
         return hand.toString();
+    }
+
+    public int calculatePointValue() {
+        int temp = 0;
+        for(Card card : cards) {
+            temp += card.getNumber() ;
+        }
+        if (temp > 21){
+            for (Card card : cards) {
+                if(card.getValue().equals("A") && card.getNumber() == 11 && temp > 21) {
+                    card.setNumber(1);
+                    temp -= 10;
+                }
+            }
+        }
+        return temp;
     }
 }
