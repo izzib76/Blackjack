15154f9b833b7d6825b5f33cc7e598dce74eb8f2
Izzi Bikun <izzi.bikun@kroger.com>
2018-08-23 16:21:48 -0400

    BLKJK-007 | Izzi | Calculate point value of hand, accounting for Aces

--- src/com/izzib76/blackjack/model/Card.java
+++ src/com/izzib76/blackjack/model/Card.java
@@ -17,6 +17,16 @@ public class Card {
     }
 
     /**
+     * @param s suit
+     * @param v face value
+     */
+    public Card(String s, String v) {
+        this.suit = s;
+        this.value = v;
+        this.number = determinePointValueFromFaceValue(v);
+    }
+
+    /**
      * @return the suit of the card
      */
     public String getSuit() {
@@ -56,6 +66,16 @@ public class Card {
      */
     public void setNumber(int n) {
         this.number = n;
+    }
+
+    public int determinePointValueFromFaceValue(String faceValue){
+        switch(faceValue) {
+            case "K" : return 10;
+            case "Q" : return 10;
+            case "J" : return 10;
+            case "A" : return 11;
+            default : return new Integer(faceValue);
+        }
     }
 
     public String toString(){
